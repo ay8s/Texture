9874ab32efa802648fcf5e8052a8ae3592471923
Andrew Yates <contact@andydev.co.uk>
2024-04-11 12:54:08 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -56,7 +56,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pinterest/PINRemoteImage.git", .upToNextMajor(from: "3.0.3")),
-        .package(url: "https://github.com/Instagram/IGListKit", revision: "c2cea36534fe08e28d7c060495970785cd956593"),
+        .package(url: "https://github.com/Instagram/IGListKit", branch: "main"),
     ],
     targets: [
         .target(
