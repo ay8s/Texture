8e545df70a773b9cb666b9bfdfc129a9717f88c5
Andrew Yates <contact@andydev.co.uk>
2024-04-11 19:02:05 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,6 +23,8 @@ let sharedDefines: [CSetting] = [
     
                                 // PINRemoteImage always available for Texture
                                 .define("AS_PIN_REMOTE_IMAGE", to: "1"),
+
+                                .define("AS_USE_VIDEO", to: "1"),
                                 
                                 // always disabled
                                 .define("IG_LIST_COLLECTION_VIEW", to: "0"),]
